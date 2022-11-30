375f2c59656db63e0b1df4911a7991831a6d91ef
Samir Azzam <40363140+WestsideBG@users.noreply.github.com>
2022-11-30 15:18:15 +0200

    added some js

--- src/js/app.js
+++ src/js/app.js
@@ -3,5 +3,10 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
+  let price = document.getElementsByClassName("price")[0].innerHTML
 
+  let products = document.getElementsByClassName("product")
+  for (let index = 0; index < products.length; index++) {
+    products[index].setAttribute("data-price", price) 
+  }
 });
